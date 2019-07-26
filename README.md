99597889e72ad865e58c52e4fdf0516c43a42cbe
Kim Chouard <k.chouard@salesforce.com>
2019-07-26 10:41:52 -0700

    Adding EXIF info and updated Readme

--- README.md
+++ README.md
@@ -1,2 +1,97 @@
 # whats-your-focal-lengths
 Get some more insight on what focal length you shot the most with.
+
+# Usage
+
+```node calc-focal-length.js```
+
+This will ask you a folder in which to find your JPGs.
+
+You can specify the folder directly with `--folder`:
+
+```node calc-focal-length.js --folder "/Volumes/Kim Chouard/
+Sony/"```
+
+# Outputs
+
+Create a exifs.csv with extended data on any JPG in the specified folder.
+
+## path
+Path of the image.
+
+## ExposureTime
+Exposure time given in seconds (sec). 
+## ExposureProgram
+The class of the program used by the camera to set exposure when the picture is taken. 
+
+Default = 0
+
+- 0 = Not defined
+- 1 = Manual
+- 2 = Normal program
+- 3 = Aperture priority
+- 4 = Shutter priority
+- 5 = Creative program (biased toward depth of field)
+- 6 = Action program (biased toward fast shutter speed)
+- 7 = Portrait mode (for closeup photos with the background out of focus)
+- 8 = Landscape mode (for landscape photos with the background in focus)
+- Other = reserved 
+
+## FNumber
+The F number.
+
+## ISO
+
+The ISO value.
+
+## CreateDate
+
+Format: `2019:02:13 12:57:07`
+
+## ShutterSpeedValue
+
+Shutter speed. The unit is the APEX (Additive System of Photographic Exposure) setting.
+
+## ApertureValue
+
+The lens aperture. The unit is the APEX value. 
+
+## MaxApertureValue
+
+The smallest F number of the lens. The unit is the APEX value. 
+
+## BrightnessValue
+
+The value of brightness. The unit is the APEX value. Ordinarily it is given in the range of -99.99 to 99.99. 
+
+## MeteringMode
+
+The metering mode.
+
+Default = 0
+- 0 = unknown
+- 1 = Average
+- 2 = CenterWeightedAverage
+- 3 = Spot
+- 4 = MultiSpot
+- 5 = Pattern
+- 6 = Partial
+- Other = reserved
+- 255 = other 
+
+
+## FocalLength
+
+The actual focal length of the lens in mm. Conversion is not made to the focal length of a 35 mm film camera. 
+
+## LensInfo
+
+## LensModel
+
+## SubjectDistance
+
+The distance to the subject given in meters. Note that if the numerator of the recorded value is FFFFFFFF.H Infinity shall be indicated; and if the numerator is 0 Distance unknown shall be indicated. 
+
+---
+
+Full Documentation available p.31: https://www.exif.org/Exif2-2.PDF
--- calc-focal-length.js
+++ calc-focal-length.js
@@ -6,6 +6,7 @@ const readline = require('readline').createInterface({
   input: process.stdin,
   output: process.stdout
 });
+const args = require('minimist')(process.argv.slice(2));
 
 // Create the array that contains all the images data
 let exifs = [];
@@ -29,17 +30,46 @@ const extractExifs = async (files, iterator) => {
       if (error) { return console.error('Error: ', error.message); }
       
       let exif = exifData.exif;
+      // DOC: p.31 https://www.exif.org/Exif2-2.PDF
       let newExif = {
         path: file,
-        FNumber: exif.FNumber,
+        ExposureTime: exif.ExposureTime, // Exposure time, given in seconds (sec). 
+        ExposureProgram: exif.ExposureProgram, /*
+          Default = 0
+            0 = Not defined
+            1 = Manual
+            2 = Normal program
+            3 = Aperture priority
+            4 = Shutter priority
+            5 = Creative program (biased toward depth of field)
+            6 = Action program (biased toward fast shutter speed)
+            7 = Portrait mode (for closeup photos with the background out of focus)
+            8 = Landscape mode (for landscape photos with the background in focus)
+            Other = reserved 
+        */
+        FNumber: exif.FNumber, // The F number.
         ISO: exif.ISO,
         CreateDate: exif.CreateDate,
-        ShutterSpeedValue: exif.ShutterSpeedValue,
-        ApertureValue: exif.ApertureValue,
-        MeteringMode: exif.MeteringMode,
-        FocalLength: exif.FocalLength,
+        ShutterSpeedValue: exif.ShutterSpeedValue, // Shutter speed. The unit is the APEX (Additive System of Photographic Exposure) setting
+        ApertureValue: exif.ApertureValue, // The lens aperture. The unit is the APEX value. 
+        MaxApertureValue: exif.MaxApertureValue, //The smallest F number of the lens. The unit is the APEX value. 
+        BrightnessValue: exif.BrightnessValue, // The value of brightness. The unit is the APEX value. Ordinarily it is given in the range of -99.99 to 99.99. 
+        MeteringMode: exif.MeteringMode, /* The metering mode.
+          Default = 0
+            0 = unknown
+            1 = Average
+            2 = CenterWeightedAverage
+            3 = Spot
+            4 = MultiSpot
+            5 = Pattern
+            6 = Partial
+            Other = reserved
+            255 = other 
+        */
+        FocalLength: exif.FocalLength, // The actual focal length of the lens, in mm. Conversion is not made to the focal length of a 35 mm film camera. 
         LensInfo: exif.LensInfo,
         LensModel: exif.LensModel,
+        SubjectDistance: exif.SubjectDistance, // The distance to the subject, given in meters. Note that if the numerator of the recorded value is FFFFFFFF.H, Infinity shall be indicated; and if the numerator is 0, Distance unknown shall be indicated. 
       }
 
       // console.log('Adding line:',newExif, exif);
@@ -58,41 +88,30 @@ const extractExifs = async (files, iterator) => {
     return console.error('Error: ', error.message);
   }
 }
-  
-readline.question(`What folder do you want to analyze?`, (folder) => {
-  console.log(`You chose "${folder}"`);
-  // options is optional
-  glob(`${path.join(__dirname, folder)}/**/*.jpg`, {}, (err, files) => {
-    // files is an array of filenames.
-    // If the `nonull` option is set, and nothing
-    // was found, then files is ["**/*.js"]
-    // er is an error object or null.
 
+const getFilesExifs = (folder) => {
+  console.log(`You chose "${folder}"`);
+
+  // the folder is absolute IF starts with "/", otherwise relative to the current folder.
+  glob(`${(folder.search('^\/') === -1) ? path.join(__dirname, folder) : folder}/**/*.jpg`, {}, (err, files) => {
     if (err) {
       return console.log('Unable to scan directory: ' + err);
     } 
 
     extractExifs(files, 0);
-  })
+  });
+}
 
-  
+// MAIN
+if (args['folder']) {
   readline.close();
-})
-  
-
-
-
-
-
-// var im = require('imagemagick');
-// im.readMetadata('SA307923.ARW', function(err, metadata){
-//   if (err) throw err;
-//   console.log('Shot at '+metadata.exif.dateTimeOriginal, metadata);
-// })
-// // -> Shot at Tue, 06 Feb 2007 21:13:54 GMT
-
-// const fileMetadata = require('file-metadata');
- 
-// (async () => {
-//     console.log(await fileMetadata('SA307923.ARW'));
-// })();
+  let folder = args['folder'];
+  getFilesExifs(folder);
+}
+else {
+  readline.question(`What folder do you want to analyze?`, (folder) => {
+    getFilesExifs(folder);
+    
+    readline.close();
+  });
+}
